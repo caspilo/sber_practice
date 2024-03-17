1b7ec991f7b72b7949c0a102a6f98718ca8a235f
caspilo <usov.vanya@bk.ru>
2024-03-18 01:27:04 +0500

    1 part done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,10 +11,10 @@ public class Main {
         List<City> cities = new ArrayList<>();
         try {
             Scanner scanner = new Scanner(new File("D:\\Работа\\СбеРстажировка\\sber1\\src\\main\\resources\\file_sber.csv"));
-            while (scanner.hasNextLine()){
+            while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
                 String[] parts = line.split(";");
-                if (parts.length >=5) {
+                if (parts.length >= 5) {
                     String name = parts[0].trim();
                     String region = parts[1].trim();
                     String district = parts[2].trim();
@@ -30,12 +30,12 @@ public class Main {
             e.printStackTrace();
         }
 
-        for (City city: cities){
-            System.out.println("City{" + "name=" + "'" +  city.getName() + "'" + ", "
-                    + "region=" + "'" +  city.getRegion() + "'" + ", "
-                    + "district=" + "'" +  city.getDistrict() + "'" + ", "
-                    + "population=" + "'" +  city.getPopulation() + "'" + ", "
-                    + "foundation=" + "'" +  city.getFoundation() + "'" + "}"
+        for (City city : cities) {
+            System.out.println("City{" + "name=" + "'" + city.getName() + "'" + ", "
+                    + "region=" + "'" + city.getRegion() + "'" + ", "
+                    + "district=" + "'" + city.getDistrict() + "'" + ", "
+                    + "population=" + "'" + city.getPopulation() + "'" + ", "
+                    + "foundation=" + "'" + city.getFoundation() + "'" + "}"
             );
         }
     }
